6e4d1f2ae543449a571ff102f444d6fda27b8750
mevey <muthoni90@gmail.com>
2016-08-02 13:02:36 +0300

    Step 1: Added Amchart

--- _static/assets/js/js.js
+++ _static/assets/js/js.js
@@ -89,11 +89,36 @@ jQuery(function($) {
 			return x1 + x2;
 
 		}
+        county = window.location.href.split('county=')[1]
+        updateMapData(translateIDtoTitle(county));
+        var map = AmCharts.makeChart( "chartdiv", {
+          type: "map",
+          "theme": "dark",
 
+          colorSteps: 10,
+
+          dataProvider: {
+            map: "kenyaHigh",
+            mapURL: "/lib/3/maps/svg/kenyaHigh.svg",
+            areas: MAP_DATA
+          },
+
+          areasSettings: {
+            autoZoom: false
+          },
+
+          "export": {
+            "enabled": true
+          },
+
+          zoomable: false,
+          zoomControl: { zoomControlEnabled: false, panControlEnabled: false, homeButtonEnabled: false},
+        });
 	});
 });
 
-
+COUNTY_ARR = [["KE-28","Mombasa"],["KE-19","Kwale"],["KE-39","Taita-Taveta"],["KE-94","Kilifi"],["KE-21","Lamu"],["KE-23","Makueni"],["KE-30","Nairobi"],["KE-10","Kajiado"],["KE-22","Machakos"],["KE-13","Kiambu"],["KE-27","Migori"],["KE-29","Murang%27a"],["KE-16","Kisii"],["KE-33","Narok"],["KE-34","Nyamira"],["KE-02","Bomet"],["KE-08","Homa-Bay"],["KE-06","Embu"],["KE-15","Kirinyaga"],["KE-18","Kitui"],["KE-40","Tana-River"],["KE-36","Nyeri"],["KE-17","Kisumu"],["KE-12","Kericho"],["KE-41","Tharaka"],["KE-35","Nyandarua"],["KE-45","Vihiga"],["KE-31","Nakuru"],["KE-38","Siaya"],["KE-32","Nandi"],["KE-26","Meru"],["KE-04","Busia"],["KE-20","Laikipia"],["KE-11","Kakamega"],["KE-44","Uasin-Gishu"],["KE-07","Garissa"],["KE-03","Bungoma"],["KE-42","Trans-Nzoia"],["KE-05","Elgeyo-Marakwet"],["KE-01","Baringo"],["KE-09","Isiolo"],["KE-37","Samburu"],["KE-47","West-Pokot"],["KE-46","Wajir"],["KE-24","Mandera"],["KE-25","Marsabit"],["KE-43","Turkana"]]
+MAP_DATA = [{id: 'KE-01', value: 0}, {id: 'KE-02', value: 0}, {id: 'KE-03', value: 0}, {id: 'KE-04', value: 0}, {id: 'KE-05', value: 0}, {id: 'KE-06', value: 0}, {id: 'KE-07', value: 0}, {id: 'KE-08', value: 0}, {id: 'KE-09', value: 0}, {id: 'KE-10', value: 0}, {id: 'KE-11', value: 0}, {id: 'KE-12', value: 0}, {id: 'KE-13', value: 0}, {id: 'KE-14', value: 0}, {id: 'KE-15', value: 0}, {id: 'KE-16', value: 0}, {id: 'KE-17', value: 0}, {id: 'KE-18', value: 0}, {id: 'KE-19', value: 0}, {id: 'KE-20', value: 0}, {id: 'KE-21', value: 0}, {id: 'KE-22', value: 0}, {id: 'KE-23', value: 0}, {id: 'KE-24', value: 0}, {id: 'KE-25', value: 0}, {id: 'KE-26', value: 0}, {id: 'KE-27', value: 0}, {id: 'KE-28', value: 0}, {id: 'KE-29', value: 0}, {id: 'KE-30', value: 0}, {id: 'KE-31', value: 0}, {id: 'KE-32', value: 0}, {id: 'KE-33', value: 0}, {id: 'KE-34', value: 0}, {id: 'KE-35', value: 0}, {id: 'KE-36', value: 0}, {id: 'KE-37', value: 0}, {id: 'KE-38', value: 0}, {id: 'KE-39', value: 0}, {id: 'KE-40', value: 0}, {id: 'KE-41', value: 0}, {id: 'KE-42', value: 0}, {id: 'KE-43', value: 0}, {id: 'KE-44', value: 0}, {id: 'KE-45', value: 0}, {id: 'KE-46', value: 0}, {id: 'KE-47', value: 0}]
 // get param from url
 
 function getUrlParameter(sParam) {
@@ -110,3 +135,26 @@ function getUrlParameter(sParam) {
 		}
 	}
 };
+
+function translateIDtoTitle(name) {
+    console.log(name)
+    name = name.toLowerCase();
+    for (k in COUNTY_ARR) {
+        j = COUNTY_ARR[k];
+        if (name == j[1].toLowerCase()) {
+            return j[0];
+        }
+    }
+}
+
+function updateMapData(id) {
+    for (k in MAP_DATA) {
+        j = MAP_DATA[k];
+        if (j['id'] = id) {
+            j['value'] = 100;
+        } else {
+            j['value'] = 10 * parseInt(Math.random(0,8) * 10);
+        }
+    }
+}
+
